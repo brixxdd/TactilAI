6b1c52f55008fd433fcc85f6943f36874d1b3652
Admin1234 <Admin1234>
2026-03-26 17:31:02 -0600

    Initial Commit

---
+++ TactilAI/TactilAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TactilAIApp.swift
+//  TactilAI
+//
+//  Created by ADMIN UNACH on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TactilAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
